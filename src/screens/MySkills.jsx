3be084fbcf6d239a66dacefd64205a798835db05
Kevin Samson <kevinsam2003@gmail.com>
2023-07-19 09:37:16 +0400

    made the flutter logo the proper size

--- src/screens/MySkills.jsx
+++ src/screens/MySkills.jsx
@@ -136,7 +136,7 @@ const MySkills = () => {
 
                 <svg
                   viewBox="0 0 128 128"
-                  className="hover:scale-150 transition duration-500 w-28 h-28"
+                  className="hover:scale-150 transition duration-500"
                 >
                   <g fill="#3FB6D3">
                     <path d="M12.3 64.2L76.3 0h39.4L32.1 83.6zM76.3 128h39.4L81.6 93.9l34.1-34.8H76.3L42.2 93.5z"></path>
